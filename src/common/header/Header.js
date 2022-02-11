e91ed1306d85017fd371952a7f31aa551141b736
Beenapat <beena.patil1011@gmail.com>
2022-02-11 21:11:06 +0530

    checkpoint 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import 'Header.css';
+class Header extends Component {
+	
+	<div className='attribute'>
+	<img className='logo'  src="logo.svg" alt="Italian Trulli">
+	</div>
+	
+}
