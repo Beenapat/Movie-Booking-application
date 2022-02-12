149de1431506cc7c45876ee685214d3b57d3b91e
Beenapat <beena.patil1011@gmail.com>
2022-02-12 15:55:39 +0530

    checkpoint 4

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -9,6 +9,27 @@ class Header extends Component {
 	<span><h1>Upcoming Movies</h1></span>
 	<Home/>
 	 
-	 </div>)
+	 </div>
+	
+	 
+	 	 <div>
+		 
+		 <form>
+	  <h1>FIND MOVIES BY:</h1>
+	  <input type="text" id="name" name="name" placeholder="Movie Name">
+	  <input type="text" id="genres" name="genres" placeholder="Genres">
+	  <input type="text" id="artists" name="artists" placeholder="Artists">
+	
+  <input type="date" id="Date" placeholder="Release day start">
+  <input type="date" id="Date" placeholder="Release day end">
+  
+  <button class="button button1">Apply</button>
+	 </div>
+	 
+	 </form>
+	 )
+	 
+	 
+	
 	 
 	 }
